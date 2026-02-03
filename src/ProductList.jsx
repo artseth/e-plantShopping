84fd4d106007dbd04dfd11af0e20b857fd61f77f
artseth <artsethmedia01@gmail.com>
2026-02-03 23:48:29 +0200

    ProductList.jsx

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -10,7 +10,18 @@ function ProductList({ onHomeClick }) {
         {
             category: "Air Purifying Plants",
             plants: [
-                
+                {
+                    name: "Snake Plant",
+                    image: "https://cdn.pixabay.com/photo/2021/01/22/06/04/snake-plant-5939187_1280.jpg",
+                    description: "Produces oxygen at night, improving air quality.",
+                    cost: "$15"
+                },
+                {
+                    name: "Spider Plant",
+                    image: "https://cdn.pixabay.com/photo/2018/07/11/06/47/chlorophytum-3530413_1280.jpg",
+                    description: "Filters formaldehyde and xylene from the air.",
+                    cost: "$12"
+                },
                 {
                     name: "Peace Lily",
                     image: "https://cdn.pixabay.com/photo/2019/06/12/14/14/peace-lilies-4269365_1280.jpg",

5fcc73f5868b26aa360a44e6b6d7280dbf773c47
artseth <artsethmedia01@gmail.com>
2026-02-04 22:15:29 +0200

    CartItem.jsx

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -30,7 +30,12 @@ const CartItem = ({ onContinueShopping }) => {
   const handleCheckoutShopping = (e) => {alert('Functionality to be added for future reference');
 };
 
-  const handleIncrement = (item) => {};
+  const handleIncrement = (item) => {
+    dispatch(updateQuantity({
+      name: item.name,
+      quantity: item.quantity + 1
+    }))
+  };
 
   const handleDecrement = (item) => {};
 
